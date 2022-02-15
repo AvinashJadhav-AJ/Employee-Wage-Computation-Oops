2c761ec9fc67e1388875f0bbd17cfc0f7994edec
Avinash <aj912873@gmail.com>
2022-02-16 01:34:11 +0530

    Welcome to Employee Wage Computation Program Object Orinted Programing

---
+++ src/com/Bridgelabz/java/EmployeeWageComputation.java
@@ -0,0 +1,7 @@
+package com.Bridgelabz.java;
+
+public class EmployeeWageComputation {
+	public static void main(String []Args) {
+		System.out.println("Welcome to Employee Wage Computation Program Object Orinted Programing");
+	}
+}
